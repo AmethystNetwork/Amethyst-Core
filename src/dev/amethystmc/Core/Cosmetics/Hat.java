6d9f348c73131041f9f229208bf20465450bcfad
DevGolem <golemdeveloper@gmail.com>
2016-09-26 00:38:58 -0400

    Started on hats, but then got sleepy

---
+++ src/dev/amethystmc/Core/Cosmetics/Hat.java
@@ -0,0 +1,34 @@
+package dev.amethystmc.Core.Cosmetics;
+
+import dev.amethystmc.Core.Ranks.Rank;
+import org.bukkit.Material;
+
+/**
+ * Created by Ben on 9/26/2016.
+ */
+public enum Hat
+{
+
+    GLASS("Glass", "&dGlass Hat", Material.GLASS, Rank.PREMIUM, 10),
+    SPONGE("Sponge", "&dSponge Hat", Material.SPONGE, Rank.PREMIUM, 11),
+    CACTUS("Cactus", "&Cactus Hat", Material.CACTUS, Rank.PREMIUM, 12);
+
+    String name;
+    String displayName;
+    Material material;
+    Rank requiredRank;
+    int slot;
+
+    Hat(String name, String displayName, Material material, Rank requiredRank, int slot)
+    {
+
+        this.name = name;
+        this.displayName = displayName;
+        this.material = material;
+        this.requiredRank = requiredRank;
+        this.slot = slot;
+
+    }
+
+}
+
