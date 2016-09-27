f8f2e7bde917ba9e8f006a39e239106b9b43e6de
DevGolem <golemdeveloper@gmail.com>
2016-09-27 00:09:33 -0400

    More changes...
    Just a very small line added

--- src/dev/amethystmc/Core/Interfaces/CosmeticInterfaces/HatInterface.java
+++ src/dev/amethystmc/Core/Interfaces/CosmeticInterfaces/HatInterface.java
@@ -1,7 +1,9 @@
 package dev.amethystmc.Core.Interfaces.CosmeticInterfaces;
 
 import dev.amethystmc.Core.Utils.InterfaceUtil;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
 
 /**
  * Created by Ben on 9/26/2016.
@@ -17,6 +19,10 @@ public class HatInterface extends InterfaceUtil
 
     @Override
     public void open(Player p) {
+
+        Inventory inv = Bukkit.createInventory(p, 45, getName());
+
+        p.openInventory(inv);
 
     }
 
