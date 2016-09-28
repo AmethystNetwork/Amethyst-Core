fc6fd66fcbd3f468c72aea6107d0bf92a2a7f39f
DevGolem <golemdeveloper@gmail.com>
2016-09-27 23:20:20 -0400

    Added a death message
    ^^

--- src/dev/amethystmc/Core/Cosmetics/DeathMessage.java
+++ src/dev/amethystmc/Core/Cosmetics/DeathMessage.java
@@ -11,7 +11,8 @@ import org.bukkit.inventory.ItemStack;
 public enum DeathMessage
 {
 
-    YOU_GOT_ME_THIS_TIME("You got me this time", "&bYou got me this time!", ItemUtil.createItem(Material.ANVIL, "&bYou got me this time!"), Rank.PREMIUM, 11);
+    YOU_GOT_ME_THIS_TIME("You got me this time", "&bYou got me this time!", ItemUtil.createItem(Material.ANVIL, "&bYou got me this time!"), Rank.PREMIUM, 11),
+    YOLO("You Only Live Once", "&cYou Only Live Once!", ItemUtil.createItem(Material.ANVIL, "&cYou Only Live Once!"), Rank.PREMIUM, 12);
 
     String name;
     String deathMessage;
