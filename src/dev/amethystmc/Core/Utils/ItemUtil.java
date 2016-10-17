ba4d884e61051f4dc2e62dd8f8ada505980c7d95
DevGolem <golemdeveloper@gmail.com>
2016-10-17 19:08:33 -0400

    Changes...

--- src/dev/amethystmc/Core/Utils/ItemUtil.java
+++ src/dev/amethystmc/Core/Utils/ItemUtil.java
@@ -1,6 +1,9 @@
 package dev.amethystmc.Core.Utils;
 
+import net.minecraft.server.v1_8_R3.NBTTagCompound;
+import net.minecraft.server.v1_8_R3.NBTTagList;
 import org.bukkit.Material;
+import org.bukkit.craftbukkit.v1_8_R3.inventory.CraftItemStack;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.SkullMeta;
@@ -162,6 +165,23 @@ public class ItemUtil
         item.setItemMeta(meta);
 
         return item;
+    }
+
+    public static ItemStack addGlow(ItemStack item)
+    {
+        net.minecraft.server.v1_8_R3.ItemStack nmsStack = CraftItemStack.asNMSCopy(item);
+        NBTTagCompound tag = null;
+        if (!nmsStack.hasTag()) {
+            tag = new NBTTagCompound();
+            nmsStack.setTag(tag);
+        }
+        if (tag == null) {
+            tag = nmsStack.getTag();
+        }
+        NBTTagList ench = new NBTTagList();
+        tag.set("ench", ench);
+        nmsStack.setTag(tag);
+        return CraftItemStack.asCraftMirror(nmsStack);
     }
 }
 
